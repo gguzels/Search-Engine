d297cec489a7ace7fe34e8c21ff5b3939e192c41
Sefa Guzel <gguzels@hotmail.com>
2023-01-07 22:40:15 +0300

    arama_motoru

---
+++ searchEngine.js
@@ -0,0 +1,29 @@
+$(function () {
+	$("#search-input").keyup(function () {
+		console.log($("#search-input").val().length);
+		if ($(this).val().length >= 2) {
+			data = "kelime=" + $("#search-input").val();
+			$.ajax({
+				type: "POST",
+				data: data,
+				url: "arama.php",
+				success: function (sonuc) {
+					$("#sonuclar").html(sonuc);
+				},
+				error: function (hata) {
+					$("#sonuclar").html(hata);
+				}
+			});
+		}
+	});
+
+	$("#search-input").focus(function () {
+		if ($("#search-input").val().length >= 2) {
+			$("#sonuclar").css("visibility", "visible");
+		}
+	});
+
+	$("#search-input").blur(function () {
+		$("#sonuclar").css("visibility", "hidden");
+	});
+});
